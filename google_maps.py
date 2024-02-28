193cc0640be25201b3859a9a0d95712e6f664a18
kevincardenas990 <140980582+kevincardenas990@users.noreply.github.com>
2024-02-27 19:06:30 -0700

    Gdirections getter and setter

--- google_maps.py
+++ google_maps.py
@@ -8,21 +8,22 @@ from constants import KEY
 class Gdirections():
     def __init__(self, start=None, end=None) -> None:    
         self.gmaps = googlemaps.Client(KEY)
-        self.start = start
-        self.end = end
+        self._start = start
+        self._end = end
         self.start_time = datetime.now()
         self._raw_data = self.get_raw_data()
+
 
     def get_current_location(self):
         '''Unsure how you'd be able to'''
         pass
 
     def get_raw_data(self, mode="driving"):
-        print(self.start, self.end)
-        if self.start is None or self.end is None:
+        print(self._start, self._end)
+        if self._start is None or self._end is None:
             return None
         
-        data = self.gmaps.directions(self.start, self.end, mode=mode, departure_time=self.start_time)
+        data = self.gmaps.directions(self._start, self._end, mode=mode, departure_time=self.start_time)
         if data:
             return data[0]["legs"][0]
         else:
@@ -33,12 +34,34 @@ class Gdirections():
         return self._raw_data["duration_in_traffic"]['text']
     
     def update_info(self, start, end ):
-        self.start, self.end =start, end
+        self._start, self._end = start, end
         self._raw_data=self.get_raw_data()
 
     def write_txt(self, items):
         with open("Google_Json.txt", 'w') as in_file:
                 in_file.write(str(items))
+
+    @property
+    def start(self):
+        return self._start
+    
+    @start.setter
+    def start(self, item):
+        if not isinstance(item, str):
+            raise ValueError ("Please enter a String")
+        self._start = item
+
+    @property
+    def end(self):
+        return self._end
+    
+    @end.setter
+    def end(self, item):
+        if not isinstance(item, str):
+            raise ValueError ("Please enter a String")
+        self._end = item
+
+
 
 if __name__ == "__main__":
     direction = Gdirections("818 E 500 S American Fork", "1001 Campus Dr Orem")
